da438b4e8dfb0f5959b08062dbb0d29013ba5b18
Jan Samak <Jan.Samak@samak-it.de>
2017-03-20 18:51:25 +0100

    Added Basic Project Master

---
+++ TSApp/TSApp/TSApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace TSApp.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
